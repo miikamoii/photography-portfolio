e7c844ea0e91a2c1d6ee0710653bee858044d04d
miikamoii <miimoi@hotmail.com>
2025-08-04 10:27:36 +0300

    Prevent theme flash and add smooth transitions for theme toggle and initial load

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import {
   ThemeProvider as NextThemesProvider,
   ThemeProviderProps,
@@ -9,5 +10,9 @@ export default function ThemeProvider({
   children,
   ...props
 }: ThemeProviderProps) {
+  useEffect(() => {
+    document.documentElement.classList.add("ready");
+  }, []);
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
