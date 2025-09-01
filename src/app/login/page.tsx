2102a774cdd12be4361e17d658d6e8c57c27c43b
miikamoii <miimoi@hotmail.com>
2025-09-01 15:55:10 +0300

    Update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,14 @@ import AuthForm from "@/components/AuthForm";
 export default function LoginPage() {
   return (
     <main className="flex min-h-screen items-center justify-center px-4">
-      <AuthForm />
+      <div className="w-full max-w-md space-y-6">
+        <p className="text-sm text-center text-gray-500 dark:text-gray-400">
+          ⚠️ This login / register is temporary and only used to showcase user
+          authentication for accessing the before/after tool. <br></br> Accounts
+          are deleted after 1 hour.
+        </p>
+        <AuthForm />
+      </div>
     </main>
   );
 }
