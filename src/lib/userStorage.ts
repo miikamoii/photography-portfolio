f5f9aa24c9874c1e36c9fb95df1ee0cae94ad993
miikamoii <miimoi@hotmail.com>
2025-09-03 12:23:30 +0300

    Update userStorage.ts

--- src/lib/userStorage.ts
+++ src/lib/userStorage.ts
@@ -11,8 +11,8 @@ type StoredUser = {
 };
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
 });
 
 function redisKey(username: string) {
